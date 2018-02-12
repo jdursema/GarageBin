2033f29c9bbca4b9073c3e9fcaadb38824b885c4
Julie Dursema <jmdursema@gmail.com>
2018-02-12 10:41:41 -0700

    set up test environment

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,17 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/testgarage',
+    migrations:{
+      directory: './db/migrations'
+    },
+    seeds:{
+      directory:'./db/seeds/test'
+    },
+    useNullAsDefault: true
   }
 
 };
