876519f8a53bbfaf519a6afb379d237253cdfcac
Julie Dursema <jmdursema@gmail.com>
2018-02-12 17:24:23 -0700

    patch when user changes cleanliness rating

--- public/js/script.js
+++ public/js/script.js
@@ -3,14 +3,22 @@ const fetchItems = async(event) => {
 
   const initialFetch = await fetch('/api/v1/stuff')
   const fetchResponse = await initialFetch.json()
+  const cleanlinessArray=['Sparkling', 'Dusty', 'Rancid']
+
 
   fetchResponse.stuff.forEach(item => {
+    const nonselectedCleanliness = cleanlinessArray.filter(cleanType => cleanType !== item.cleanliness)
     $('.list-of-stuff').append(`
     <div id=${item.id}>
       <h2 class='name'>${item.name}</h2>
       <div class='item-info hidden'>
         <h3>${item.reason_for_linger}</h3>
-        <h3 class='itemCleanliness'>${item.cleanliness}</h3>
+        <select name="text" class='cleanliness-selector'>
+          <option value=${item.cleanliness}>${item.cleanliness}</option>
+          <option value=${nonselectedCleanliness[0]}>${nonselectedCleanliness[0]}</option>
+          <option value=${nonselectedCleanliness[1]}>${nonselectedCleanliness[1]}</option>
+        </select>
+  
       </div>
     </div>`)
   })
@@ -42,9 +50,14 @@ const toggelItemInfo = () => {
 
 const postNewItem = async() => {
   event.preventDefault()
+ 
+
   let itemName = $('.name-input').val()
   let itemCleanliness = $('.cleanliness-selector').val()
   let itemReason = $('.reason-input').val()
+  const cleanlinessArray=['Sparkling', 'Dusty', 'Rancid']
+  const nonselectedCleanliness = cleanlinessArray.filter(cleanType => cleanType !== itemCleanliness)
+
   const postItem = await fetch('/api/v1/stuff', {
     method: "POST",
     headers: {
@@ -62,7 +75,12 @@ const postNewItem = async() => {
       <h2 class='name'>${itemName}</h2>
       <div class='item-info hidden'>
         <h3>${itemReason}</h3>
-        <h3 class='itemCleanliness'>${itemCleanliness}</h3>
+        <select>
+          <option value=${itemCleanliness}>${itemCleanliness}</option>
+          <option value=${nonselectedCleanliness[0]}>${nonselectedCleanliness[0]}</option>
+          <option value=${nonselectedCleanliness[1]}>${nonselectedCleanliness[1]}</option>
+        </select>
+  
       </div>
     </div>`)
     const itemsArray = []
@@ -82,19 +100,27 @@ const sortItemsAlphabetically = async() => {
   const initialFetch = await fetch('/api/v1/stuff')
   const stuffResponse = await initialFetch.json()
   const sortedItems= await stuffResponse.stuff.sort((a, b) => {
-    if(a.name < b.name) return -1;
-    if(a.name> b.name) return 1;
+    if(a.name.toUpperCase() < b.name.toUpperCase()) return -1;
+    if(a.name.toUpperCase()> b.name.toUpperCase()) return 1;
     return 0;
   })
   $('.list-of-stuff').children().remove()
 
   sortedItems.forEach(item => {
+    const cleanlinessArray=['Sparkling', 'Dusty', 'Rancid']
+    const nonselectedCleanliness = cleanlinessArray.filter(cleanType => cleanType !== item.cleanliness)
+
     $('.list-of-stuff').append(`
     <div id=${item.id}>
       <h2 class='name'>${item.name}</h2>
       <div class='item-info hidden'>
         <h3>${item.reason_for_linger}</h3>
-        <h3>${item.cleanliness}</h3>
+        <select name="text" class='cleanliness-selector'>
+          <option value=${item.cleanliness}>${item.cleanliness}</option>
+          <option value=${nonselectedCleanliness[0]}>${nonselectedCleanliness[0]}</option>
+          <option value=${nonselectedCleanliness[1]}>${nonselectedCleanliness[1]}</option>
+        </select>
+  
       </div>
     </div>`)
   })
@@ -104,26 +130,53 @@ const sortItemsBackwards = async() => {
   const initialFetch = await fetch('/api/v1/stuff')
   const stuffResponse = await initialFetch.json()
   const sortedItems= await stuffResponse.stuff.sort((a, b) => {
-    if(a.name < b.name) return -1;
-    if(a.name> b.name) return 1;
+    if(a.name.toUpperCase() < b.name.toUpperCase()) return -1;
+    if(a.name.toUpperCase()> b.name.toUpperCase()) return 1;
     return 0;
   })
   $('.list-of-stuff').children().remove()
 
   sortedItems.forEach(item => {
+    const cleanlinessArray=['Sparkling', 'Dusty', 'Rancid']
+    const nonselectedCleanliness = cleanlinessArray.filter(cleanType => cleanType !== item.cleanliness)
     $('.list-of-stuff').prepend(`
     <div id=${item.id}>
       <h2 class='name'>${item.name}</h2>
       <div class='item-info hidden'>
         <h3>${item.reason_for_linger}</h3>
-        <h3>${item.cleanliness}</h3>
+        <select name="text" class='cleanliness-selector'>
+          <option value=${item.cleanliness}>${item.cleanliness}</option>
+          <option value=${nonselectedCleanliness[0]}>${nonselectedCleanliness[0]}</option>
+          <option value=${nonselectedCleanliness[1]}>${nonselectedCleanliness[1]}</option>
+        </select>
+  
       </div>
     </div>`)
   })
 }
 
-const openGarage = () => {
+const openGarage = async() => {
   $('.garage-door').toggleClass('open')
+}
+
+const patchItem = async() => {
+  if($(event.target).hasClass('cleanliness-selector')){
+    const newCleanlinessRating = $(event.target).val()
+    const itemId = $(event.target).parent().parent().attr('id')
+    console.log(newCleanlinessRating)
+    console.log(itemId)
+
+    const postChange = await fetch(`/api/v1/stuff/${itemId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        cleanliness: newCleanlinessRating 
+      })
+    })
+  }
+
 }
 
 
@@ -136,5 +189,6 @@ $('.post-btn').on('click', postNewItem)
 $('.sort-alph').on('click', sortItemsAlphabetically)
 $('.sort-back').on('click', sortItemsBackwards)
 $('.open-btn').on('click', openGarage)
+$('.list-of-stuff').on('change', patchItem)
 
 
